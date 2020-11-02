31a7d5e51272bfc7fa670e8acd06d5e1f3c8104b
camopu <aleks.revati@gmail.com>
2020-11-02 21:31:11 +0300

    add solution 05-http-server-streams 01-file-server-get

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -1,16 +1,34 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
+
+  if (pathname.includes('/')) {
+    res.statusCode = 400;
+    res.end('Wrong path');
+  }
 
   switch (req.method) {
     case 'GET':
+      const stream = fs.createReadStream(filepath);
+
+      res.statusCode = 200;
+
+      stream.pipe(res).on('error', (err) => {
+        res.statusCode = 500;
+        res.end('Server error');
+      });
+
+      stream.on('error', (err) => {
+        res.statusCode = 404;
+        res.end('File not found');
+      });
 
       break;
 
