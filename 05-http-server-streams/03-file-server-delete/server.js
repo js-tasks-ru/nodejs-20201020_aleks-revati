efad489781927a34ea3b68bad0226744103e2d6c
camopu <aleks.revati@gmail.com>
2020-11-03 12:28:59 +0300

    add solution 05-http-server-streams 03-file-server-delete

--- 05-http-server-streams/03-file-server-delete/server.js
+++ 05-http-server-streams/03-file-server-delete/server.js
@@ -1,16 +1,38 @@
+/* eslint-disable space-before-function-paren */
+/* eslint-disable indent */
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
+
+  if (pathname.includes('/')) {
+    res.statusCode = 400;
+    res.end('Wrong path');
+  }
 
   switch (req.method) {
     case 'DELETE':
+      fs.unlink(filepath, function (err) {
+        if (err && err.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end('File not found');
+        }
+        res.statusCode = 200;
+        res.end('File was deleted');
+      });
+
+      req.on('error', function (err) {
+        if (err) {
+          res.statusCode = 500;
+          res.end('Server error');
+        };
+      });
 
       break;
 
