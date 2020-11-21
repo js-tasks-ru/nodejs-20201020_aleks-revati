3ed8ba02aba980ca5f6197a581907f58fa4d38b5
camopu <aleks.revati@gmail.com>
2020-11-21 15:17:04 +0300

    add solution 10-websockets 01-chat

--- 10-websockets/01-chat/controllers/messages.js
+++ 10-websockets/01-chat/controllers/messages.js
@@ -1,5 +1,16 @@
+const mapMessage = require('../mappers/message');
 const Message = require('../models/Message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {messages: []};
+  try {
+    const messages = await Message.find({chat: ctx.user._id}).limit(20).sort('-date');
+
+    ctx.body = {
+      messages: messages.map(mapMessage),
+    };
+
+    return next();
+  } catch (err) {
+    throw err;
+  }
 };
